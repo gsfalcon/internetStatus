a0601b01f974903184d9d1137fa458b674088bf0
Guilherme Souza <gsfalcon@proton.me>
2023-11-07 03:13:42 -0300

    new idea

--- internetStatus.py
+++ internetStatus.py
@@ -1,19 +1,15 @@
 import speedtest
-import os
+import time
 
-st = speedtest.Speedtest()
+def get_network_speed():
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    download_speed = st.download() / 1_000_000  # Convert from bits to megabits
+    upload_speed = st.upload() / 1_000_000  # Convert from bits to megabits
+    return download_speed, upload_speed
 
-def clear_console():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def print_internet_info():
-    clear_console()
-    print("Informações sobre sua conexão à internet:")
-    print("Nome da conexão: ", st.get_best_servers()["sponsor"])
-    print("IP: ", st.get_servers()["host"])
-    print("Geolocalização: ", st.get_servers()["name"])
-    print("Velocidade de download: ", st.download() / 10**6, "Mbps")
-    print("Velocidade de upload: ", st.upload() / 10**6, "Mbps")
-    print("Quantidade de dados transferidos: ", st.results.bytes_received / 10**9, "GB")
-
-print_internet_info()
+while True:
+    download_speed, upload_speed = get_network_speed()
+    print(f"Velocidade de download: {download_speed:.2f} Mb/s")
+    print(f"Velocidade de upload: {upload_speed:.2f} Mb/s")
+    time.sleep(1)  # Atualiza a cada segundo
